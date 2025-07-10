127e19624f6e82fab91cd6b008c4991990257642
Matheus Ferreira <matheusmlfg@gmail.com>
2025-07-09 22:46:37 -0300

    Adds Roles in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :api
+
+  enum role: { admin: 0, technician: 1, customer: 2 }
 end
