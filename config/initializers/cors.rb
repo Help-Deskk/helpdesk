34596dd40fcbccbd64f538f55a029d33ad7c8e7b
Matheus Ferreira <matheusmlfg@gmail.com>
2025-07-13 14:47:20 -0300

    Adds protocol to origins on CORS config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "helpdeskapp.site"
+    origins "https://helpdeskapp.site"
 
     resource "*",
       headers: :any,
