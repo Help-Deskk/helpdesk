d09327e123b00586efcb546b2305b5b7c060c77b
Matheus Ferreira <matheusmlfg@gmail.com>
2025-07-13 14:51:53 -0300

    Change URL origins on CORS config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://helpdeskapp.site"
+    origins "https://www.helpdeskapp.site"
 
     resource "*",
       headers: :any,
