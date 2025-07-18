2866c948c39dad83eace7c5ed26677ac1c65992d
Matheus Ferreira <matheusmlfg@gmail.com>
2025-07-18 13:25:31 -0300

    Adds pending tests for User model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    pending "validates user has an unique email"
+    pending "validates user can be admin"
+    pending "validates user can be technician"
+    pending "validates user can be customer"
+  end
+
+  context "associations" do
+    pending "validates user customer has many tickets"
+    pending "validates user technician has many tickets"
+    pending "validates user admin don't have tickets"
+  end
 end
