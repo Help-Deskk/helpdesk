0ab37023d1de8f9526d165b65bfbb0650c42df01
Matheus Ferreira <matheusmlfg@gmail.com>
2025-07-26 22:08:45 -0300

    Fixes linting offenses

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  # before do
+  #   User.destroy_all
+  # end
+
   context "validations" do
     pending "validates user has an unique email"
     pending "validates user can be admin"
@@ -12,5 +16,10 @@ RSpec.describe User, type: :model do
     pending "validates user customer has many tickets"
     pending "validates user technician has many tickets"
     pending "validates user admin don't have tickets"
+  end
+
+  it "creates user" do
+    user = User.create!(email: "test@test.com", password: "qwe123", role: :admin)
+    expect(user).not_to be_nil
   end
 end
