085d95fc443d07ec46441e1c8af98241b636da37
Matheus Ferreira <matheusmlfg@gmail.com>
2025-07-27 12:33:02 -0300

    Adds specs for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,25 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # before do
-  #   User.destroy_all
-  # end
+  describe "validations" do
+    it "validates uniqueness of email" do
+      create(:admin, email: "unique@example.com")
+      user = build(:admin, email: "unique@example.com")
+      expect(user).not_to be_valid
+      expect(user.errors[:email]).to include("has already been taken")
+    end
 
-  context "validations" do
-    pending "validates user has an unique email"
-    pending "validates user can be admin"
-    pending "validates user can be technician"
-    pending "validates user can be customer"
+    it "allows creating a user with admin role" do
+      user = build(:admin)
+      expect(user).to be_valid
+    end
+
+    it "allows creating a user with technician role" do
+      user = build(:technician)
+      expect(user).to be_valid
+    end
+
+    it "allows creating a user with customer role" do
+      user = build(:customer)
+      expect(user).to be_valid
+    end
   end
 
-  context "associations" do
-    pending "validates user customer has many tickets"
-    pending "validates user technician has many tickets"
-    pending "validates user admin don't have tickets"
-  end
-
-  it "creates user" do
-    user = User.create!(email: "test@test.com", password: "qwe123", role: :admin)
-    expect(user).not_to be_nil
+  describe "creation" do
+    it "successfully creates an admin user" do
+      user = create(:admin, email: "test@test.com", password: "qwe123")
+      expect(user).to be_persisted
+    end
   end
 end
